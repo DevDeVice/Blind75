24b1dad331731c5631b32b6b894e9baa10b7d139
DevDeVice <sejkop@gmail.com>
2024-05-05 23:32:30 +0200

    readme

--- Blind75/obj/Debug/net8.0/Blind75.AssemblyInfo.cs
+++ Blind75/obj/Debug/net8.0/Blind75.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blind75")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81cb7b5689b0124867e4ca02be51f166323094f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blind75")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blind75")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
