2c41aca4d6f31f67637e693ad2433dc96f005352
DevDeVice <sejkop@gmail.com>
2024-05-08 21:44:11 +0200

    Maximum-subarray

--- Blind75/Blind75.cs
+++ Blind75/Blind75.cs
@@ -97,6 +97,26 @@ namespace Blind75
             }
             return stack.Count == 0;
         }
+        //Maximum-subarray
+        public int MaxSubArray(int[] nums)
+        {
+            int sum = 0;
+            int maxSum = nums[0];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if (nums[i] > sum)
+                {
+                    sum = nums[i];
+                }
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+            }
+            return maxSum;
+        }
     }
 }
  
