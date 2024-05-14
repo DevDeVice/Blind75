b5f11b0358d0e93f513a26b6ae114d07eaf6dde2
DevDeVice <sejkop@gmail.com>
2024-05-14 14:13:57 +0200

    Reverse-linked-list

--- Blind75/Blind75.cs
+++ Blind75/Blind75.cs
@@ -117,6 +117,37 @@ namespace Blind75
             }
             return maxSum;
         }
+        //Reverse-linked-list
+        public class ListNode
+            {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+                {
+                    this.val = val;
+                    this.next = next;
+                }
+        }
+        public class Solution
+        {
+            public ListNode ReverseList(ListNode head)
+            {
+                ListNode prev = null;
+                ListNode current = head;
+                ListNode nextNode = null;
+
+                while (current != null)
+                {
+                    nextNode = current.next; // Save the next node
+                    current.next = prev; // Reverse the link
+                    prev = current; // Move pointers one position ahead
+                    current = nextNode;
+                }
+
+                return prev;
+            }
+        }
+        //Linked-list-cycle
     }
 }
  
